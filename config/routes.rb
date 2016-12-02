08f1bd68da6012edfab5ddca82fa30ec4a0f91d7
Rails Udemy Course <rubyemaildsdsdsd@gmail.com>
2016-12-02 02:52:33 +0000

    Created pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
-  root 'welcome#home'
-  get 'about', to: 'welcome#about'
+  root 'pages#home'
+  get 'about', to: 'pages#about'
   
   resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
