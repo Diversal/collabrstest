281ba7320ae3bb0469b86836ce090fccbedf7925
Rails Udemy Course <rubyemaildsdsdsd@gmail.com>
2016-12-02 20:07:01 +0000

    Create homepage and added footer links

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201035433) do
+ActiveRecord::Schema.define(version: 20161202032636) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
