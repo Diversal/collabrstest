33edb975acd6f56afa668b335d5d7e49f7404508
Rails Udemy Course <rubyemaildsdsdsd@gmail.com>
2016-12-02 00:44:24 +0000

    Create articles table, model and add validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116041518) do
+ActiveRecord::Schema.define(version: 20161201035433) do
 
-# Could not dump table "articles" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
