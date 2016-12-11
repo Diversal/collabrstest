e636d718626daaa360f11ad7fd52a46fc293ab4f
Rails Udemy Course <rubyemaildsdsdsd@gmail.com>
2016-12-11 21:20:14 +0000

    Add users listing and update show article

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+  
   def new
     @user = User.new
   end
@@ -21,12 +26,15 @@ class UsersController < ApplicationController
      @user = User.find(params[:id])
      if @user.update(user_params)
        flash[:success] = "Your accunt was updated succesfully"
-       redirect_to articles_path
+       redirect_to articles_path 
      else
        render 'edit'
      end
    end
    
+   def show 
+     @user = User.find(params[:id])
+   end
   private 
   def user_params
     params.require(:user).permit(:username, :email, :password)
